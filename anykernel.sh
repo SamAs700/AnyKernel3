824d02109dce46489932ba39d7a70768d3fdb869
johnmart19 <johnivan19999@gmail.com>
2020-11-17 11:30:52 +0200

    Onclite: Zeus Kernel Anykernel3 bringup

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=Zeus kernel by Ragy365 and johnmart19
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
+device.name1=onc
+device.name2=onclite
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10.0-11.0
+supported.versions=9.0-11.0
 supported.patchlevels=
 '; } # end properties
 
